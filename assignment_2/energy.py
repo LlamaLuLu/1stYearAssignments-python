07157513da319bafcfc7ba19ba17a9237824cec1
Lulama Lingela <lulamahana@gmail.com>
2023-06-19 01:19:36 +0200

    Create energy.py

---
+++ assignment_2/energy.py
@@ -0,0 +1,10 @@
+# program to input values m and c, then calculate and output value of energy quantity
+# Lulama Lingela
+# 19 February 2023
+
+m = int(input("Enter the value of m: "))
+c = int(input("\nEnter the value of c: "))
+
+energy = m * (c ** 2)
+
+print(f"\nThe value of energy, E, is: {energy}")
